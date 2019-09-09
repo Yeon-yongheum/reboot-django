df4f42b5de99c798f17d084c2e56bfc35d8b4dea
Yeon-yongheum <yeon931@naver.com>
2019-09-09 16:18:29 +0900

    ㅇㅇ?

--- articles/models.py
+++ articles/models.py
@@ -6,4 +6,4 @@ class Article(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
-    update_at = models.DateField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
